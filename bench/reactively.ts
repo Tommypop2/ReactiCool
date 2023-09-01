b2a75aa30e57b64f7ff7d4eee9b84811cfe6c91b
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-09-01 17:31:35 +0100

    Format

--- bench/reactively.ts
+++ bench/reactively.ts
@@ -1,10 +1,10 @@
 import { reactive } from "@reactively/core";
 
 export const bench = (updates: number) => {
-    const data = reactive(0);
+	const data = reactive(0);
 	const B = reactive(() => data.get());
-    const C = reactive(() => B.get());
-    const D = reactive(() => C.get());
+	const C = reactive(() => B.get());
+	const D = reactive(() => C.get());
 	for (let i = 0; i < updates; i++) {
 		data.set(i);
 	}
