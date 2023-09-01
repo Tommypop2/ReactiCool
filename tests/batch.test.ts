5364b6f438001fd6c81dd6084d72a16b1f16092b
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-09-01 12:15:57 +0100

    Add test for proper update merging

--- tests/batch.test.ts
+++ tests/batch.test.ts
@@ -16,4 +16,21 @@ describe("batch", () => {
 		});
 		expect(updates).toBe(2);
 	});
+	test("No node is updated multiple times (updates are merged properly)", () => {
+		const [A, setA] = createSignal(0);
+		const [B, setB] = createSignal(0);
+		let updates = 0;
+		createEffect(() => {
+			A();
+			B();
+			updates++;
+		});
+		expect(updates).toBe(1);
+		batch(() => {
+			setA(1);
+			setB(1);
+			expect(updates).toBe(1);
+		});
+		expect(updates).toBe(2);
+	});
 });
