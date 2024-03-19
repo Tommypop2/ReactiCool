354dea47d515c06a4e6ffc09021036c4480cd5cb
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-03-19 22:32:53 +0000

    Remove unnecessary eagermemo

--- packages/web/src/rxcore/index.ts
+++ packages/web/src/rxcore/index.ts
@@ -12,15 +12,7 @@ const createComponent = (Comp: any, props: any) => {
 	return Comp(props);
 };
 const createRoot = (fn: () => any) => fn();
-// const eagerMemo = (fn: () => any) => {
-// 	const [get, set] = createSignal(undefined);
-// 	let res: any;
-// 	createEffect(() => {
-// 		res = fn();
-// 		set(res);
-// 	});
-// 	return get;
-// };
+
 export {
 	createSignal as signal, // Not needed for DOM expressions
 	createRoot as root,
